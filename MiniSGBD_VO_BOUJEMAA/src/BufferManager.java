fe61a6cfe9c675517ab4b0c174cc6125da7f3ba1
thanh vo.u-paris <61943753+congvtc32012@users.noreply.github.com>
2020-12-03 00:06:21 +0100

    Ajoute les methodes

--- MiniSGBD_VO_BOUJEMAA/src/BufferManager.java
+++ MiniSGBD_VO_BOUJEMAA/src/BufferManager.java
@@ -1,4 +1,17 @@
 
 public class BufferManager {
+	
+	
+	 public buff getPage(int pageId) {
+		 
+	 }
+	 
+	 public void freePage(int pageId, int valdirty) {
+		 
+	 }
+	 
+	 public void flushAll() {
+		 
+	 }
 
 }
